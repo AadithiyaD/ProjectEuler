0de768a8ea663fd766316f5582834d21866b3393
AadithiyaD <daadithiya21@gmail.com>
2025-01-06 19:21:57 +0000

    Update prob15.py

--- PythonAttempts/prob15.py
+++ PythonAttempts/prob15.py
@@ -3,7 +3,7 @@ to finish a nxn grid, 2n moves need to be made : n down, and n right
 
 Therefore the question is to find out how many ways these 2n moves can be arranged in an nxn grid
 
-In other words, calculate n C k = n!/(n!)(n-k)!, therefore here: 2n C n = 2n!/(n!)(n!)
+In other words, calculate n C k = n!/(k!)(n-k)!, therefore here: 2n C n = 2n!/(n!)(n!)
 
 '''
 
