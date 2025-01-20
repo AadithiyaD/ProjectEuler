01390396d59169d768e60773c7f2f67a36bc0cc7
AadithiyaD <daadithiya21@gmail.com>
2025-01-20 23:10:41 +0000

    Create prob23.py

---
+++ PythonAttempts/prob23.py
@@ -0,0 +1,47 @@
+'''
+Find the sum of numbers upto 28123 that cannot be expressed as the sum of two abundant numbers
+'''
+from math import sqrt
+
+limit = 28123
+
+def divisor_sums(n):
+    """
+    Function to calculate sum of divisors of the number n
+    """
+    divisors = set()
+    divisors.add(1) # In the problem, the number itself is not conted, so we manually add 1 to avoid listing the number itself as
+                    # a factor
+    for i in range(2, int(sqrt(n)) + 1):
+        if n % i == 0:
+            divisors.add(i)
+        if i != n // i:
+            divisors.add(n // i) # If i divides n, then n // i also divides n
+    
+    return sum(divisors)
+
+def abundant_nums(limit):
+    nums = [x for x in range(1,limit + 1) if divisor_sums(x) > x]
+    return nums
+
+
+input_numbers = abundant_nums(limit)
+
+def abundant_sums(n):
+    result = set()
+    for i in range(len(input_numbers)):
+        for j in range(i, len(input_numbers)):
+            s = input_numbers[i] + input_numbers[j]
+            if s <= n:
+                result.add(s)
+            else:
+                break
+
+    return result
+
+
+all_nums = set(range(1, limit + 1))
+answer = sum(all_nums - abundant_sums(limit))
+
+abundant_numbers = abundant_nums(28123)
+print(len(abundant_numbers))
